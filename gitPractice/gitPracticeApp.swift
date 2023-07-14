f315bd18baba6add97a87072176e42ad78e00a84
Thais <thaisisabelestrada@gmail.com>
2023-07-14 10:35:33 -0400

    Initial Commit

---
+++ gitPractice/gitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeApp.swift
+//  gitPractice
+//
+//  Created by Thais Estrada-Nunez on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct gitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
